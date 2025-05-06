0b62ca392aa3a10ff3d8821594357a40d026722a
Jaishikha-Sharma <jaishikhasharma05@gmail.com>
2025-05-06 19:46:09 +0530

    code

--- Server.js
+++ Server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://auth-backend-ombp.onrender.com" , "https://labs-ebon.vercel.app" , "https://www.ecmalab.com"],
+  origin: [],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
